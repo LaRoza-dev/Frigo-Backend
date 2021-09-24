49bd503f77892d4be476b99d4141c84acbfa412a
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-09-24 14:43:01 +0300

    ping request added for checking server up and checking expired token

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes.recipes.recipe import recipe_router as recipeRouter
 from auth.jwt_bearer import JWTBearer
 from routes.users.login import user_login_router as UserLoginRouter
 from routes.users.google import google_route as GoogleRouter
-from routes.recipes.ingredients import user_fridge  as UserFridge, user_ingredients as CustomIngredient
+from routes.recipes.ingredients import user_fridge as UserFridge, user_ingredients as CustomIngredient
 from routes.admins.login import admin_router as AdminRouter
 from routes.admins.users import user_router as UserRouter
 from starlette.middleware.sessions import SessionMiddleware
@@ -25,18 +25,27 @@ app.add_middleware(SessionMiddleware, secret_key=config("secret"))
 
 token_listener = JWTBearer()
 
-app.include_router(recipeRouter, tags=["Recipe"], prefix="/recipe", dependencies=[Depends(token_listener)])
+app.include_router(recipeRouter, tags=[
+                   "Recipe"], prefix="/recipe", dependencies=[Depends(token_listener)])
 app.include_router(AdminRouter, tags=["Administrators"], prefix="/admin")
 app.include_router(UserLoginRouter, tags=["User Signup and Login"])
-app.include_router(UserRouter, tags=["Users"], prefix="/users", dependencies=[Depends(token_listener)])
+app.include_router(UserRouter, tags=[
+                   "Users"], prefix="/users", dependencies=[Depends(token_listener)])
 app.include_router(GoogleRouter, tags=["google"], prefix="/google")
-app.include_router(UserFridge, tags=["fridge"], prefix="/users/fridge",dependencies=[Depends(token_listener)])
-app.include_router(CustomIngredient, tags=["custom ingredients"], prefix="/users/ingredients",dependencies=[Depends(token_listener)])
+app.include_router(UserFridge, tags=[
+                   "fridge"], prefix="/users/fridge", dependencies=[Depends(token_listener)])
+app.include_router(CustomIngredient, tags=[
+                   "custom ingredients"], prefix="/users/ingredients", dependencies=[Depends(token_listener)])
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
 
+
+@app.get("/ping", dependencies=[Depends(token_listener)])
+async def read_root():
+    return {"message": "pong!"}
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", port=8000)
